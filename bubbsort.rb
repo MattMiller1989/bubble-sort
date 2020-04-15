1a78cc4dbff8753357228b00d4991dad0c3a81a7
Matt Miller <mattmiller0989@gmail.com>
2020-04-14 21:55:20 -0500

    Added the sort by method

--- bubbsort.rb
+++ bubbsort.rb
@@ -20,5 +20,28 @@ def bubble_sort(arr)
      arr
 end
 
+
+
+def bubble_sort_by(arr)
+    sorted =true
+    while sorted 
+        sorted = false
+        (0...arr.length-1).each do |ind|
+            if yield(arr[ind],arr[ind+1])>0
+                arr[ind],arr[ind+1]=arr[ind+1],arr[ind]
+                sorted=true
+            end
+        end
+    end
+    puts arr
+    
+end
+
+
+
 puts bubble_sort([4,3,78,2,0,2])
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
+
 
